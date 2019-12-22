9a1eb2de8ea3afb210c8f889d0488c0b7ae09021
Ivan Gagis <igagis@gmail.com>
2019-12-22 14:50:57 +0200

    erunda

---
+++ tests/erunda/main.cpp
@@ -0,0 +1,46 @@
+#include <assert.h>
+#include <stdarg.h>
+#include <stdio.h>
+#include <stdint.h>
+#include <malloc.h>
+#include <stdlib.h>
+
+void *read_stack(uint8_t *out, unsigned amount, ...)
+{
+    va_list a;
+    va_start(a, amount);
+    uint8_t *start = (uint8_t *) a;
+    for (unsigned i=0; i<amount; ++i)
+        out[i] = start[i];
+    va_end(a);
+    return start;
+}
+
+void dump_stack(unsigned amount)
+{
+    assert(amount > 0);
+    uint8_t *buf = (uint8_t *) malloc(amount);
+    assert(buf != NULL);
+    void *start = read_stack(buf, amount, 239, 240, 241, 242, 243);
+    printf("Dumping %u bytes from address %p:\n", amount, start);
+    for (unsigned i=0; i<amount; i++)
+    {
+        if (i%16==0)
+            printf("[%04X] %p: ", i, (uint8_t *)start+i);
+        else if (i%4==0)
+            printf(".");
+        printf("%02X", buf[i]);
+        if (i%16==15 || i+1==amount)
+            printf("\n");
+    }
+    printf("Bye\n");
+}
+
+int main(int argc, char **argv)
+{
+    // assert(argc > 1);
+    unsigned amount = 1000;//(unsigned) atol(argv[1]);
+    printf("Hello world, main=%p\n", main);
+    dump_stack(amount);
+    return 0;
+}
