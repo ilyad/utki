6d00e564d7c55f6f486334fc33c05154075d70d2
Ilya Dogolazky <ilya.dogolazky@korulab.com>
2019-12-23 12:29:47 +0200

    some stuff

--- tests/erunda/main.cpp
+++ tests/erunda/main.cpp
@@ -5,15 +5,16 @@
 #include <malloc.h>
 #include <stdlib.h>
 
-void *read_stack(uint8_t *out, unsigned amount, ...)
+uintptr_t read_stack(uint8_t *out, unsigned amount, ...)
 {
     va_list a;
     va_start(a, amount);
     uint8_t *start = (uint8_t *) &out;
+    printf("start=%p\n", start);
     for (unsigned i=0; i<amount; ++i)
         out[i] = start[i];
     va_end(a);
-    return start;
+    return (uintptr_t) start;
 }
 
 void dump_stack(unsigned amount)
@@ -21,12 +22,12 @@ void dump_stack(unsigned amount)
     assert(amount > 0);
     uint8_t *buf = (uint8_t *) malloc(amount);
     assert(buf != NULL);
-    void *start = read_stack(buf, amount, 239, 240, 241, 242, 243);
-    printf("Dumping %u bytes from address %p:\n", amount, start);
+    uintptr_t start = read_stack(buf, amount, 239, 240, 241, 242, 243);
+    printf("Dumping %u bytes from address 0x%lu:\n", amount, start);
     for (unsigned i=0; i<amount; i++)
     {
         if (i%16==0)
-            printf("[%04X] %p: ", i, (uint8_t *)start+i);
+            printf("[%04X] 0x%lu: ", i, start+i);
         else if (i%4==0)
             printf(".");
         printf("%02X", buf[i]);
@@ -38,9 +39,8 @@ void dump_stack(unsigned amount)
 
 int main(int argc, char **argv)
 {
-    // assert(argc > 1);
-    unsigned amount = 1000;//(unsigned) atol(argv[1]);
-    printf("Hello world, main=%p\n", main);
+    unsigned amount = 0x1234;
+    printf("Hello world, main=%p, sizeof(int)=%lu, sizeof(long)=%lu, sizeof(void*)=%lu\n", main, sizeof(int), sizeof(long), sizeof(void*));
     dump_stack(amount);
     return 0;
 }
